bea929533a481f1f6a1b9658640c97b911ba4057
jayyqueue <jieqiangt@gmail.com>
2023-05-18 16:00:21 +0800

    - added font.ts set up for text & title font

---
+++ src/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Roboto_Mono } from 'next/font/google';
+
+export const textFont = Inter({
+    subsets: ['latin'],
+    variable: '--font-text',
+    display: 'swap',
+});
+
+export const titleFont = Roboto_Mono({
+    subsets: ['latin'],
+    variable: '--font-title',
+    display: 'swap',
+});
