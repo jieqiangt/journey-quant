3f37918b991490e93937bd4df1cd1da0e3da3da0
jayyqueue <jieqiangt@gmail.com>
2023-06-04 12:23:24 +0800

    - changed execute query params allowed types

--- src/utils/dbUtils.ts
+++ src/utils/dbUtils.ts
@@ -23,7 +23,7 @@ export const readSQLScript = (filePath: string) => {
   return updatedQuery;
 };
 
-export const executeQuery = async (conn: Connection, query: string, method: "insert" | "query" | "update", params: AllDBInterface | undefined = undefined) => {
+export const executeQuery = async (conn: Connection, query: string, method: "insert" | "query" | "update", params: AllDBInterface | undefined | { [colName: string]: string } = undefined) => {
   const result = params
     ? await conn.execute(query, params)
     : await conn.execute(query);
