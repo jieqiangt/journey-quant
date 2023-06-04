0e61b35740d3163f276531272a9b0567ea9a6e0e
jayyqueue <jieqiangt@gmail.com>
2023-06-04 12:23:37 +0800

    - added add months functionality

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -11,3 +11,29 @@ export const addYears = (date: Date, years: number) => {
   newDate.setFullYear(date.getFullYear() + years);
   return newDate;
 }
+
+
+export const addMonths = (date: Date, months: number) => {
+  const newDate = new Date(date)
+
+  const curMonth = newDate.getMonth() + 1
+  const posNeg = months > 0 ? 1 : -1;
+  const monthsLeft = Math.abs(months) % 12
+  let yearsToAdd = Math.floor(Math.abs(months) / 12)
+
+  let newMonth = curMonth + posNeg * monthsLeft
+
+  if (newMonth <= 0) {
+    yearsToAdd += 1
+    newMonth = 12 + (newMonth)
+  }
+
+  if (newMonth > 12) {
+    yearsToAdd += 1
+    newMonth = (newMonth) % 12
+  }
+
+  newDate.setFullYear(date.getFullYear() + posNeg * yearsToAdd);
+  newDate.setMonth(newMonth - 1)
+  return newDate;
+}
